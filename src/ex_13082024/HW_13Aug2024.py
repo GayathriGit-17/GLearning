4c81fdf90c0ad464fbe15bb915eae9054ba9b0f1
GayathriS-17 <gayathri.shanmugam@nytro.ai>
2024-08-13 11:36:48 +0530

    class aug13 with hw

--- src/ex_13082024/HW_13Aug2024.py
+++ src/ex_13082024/HW_13Aug2024.py
@@ -1,4 +1,7 @@
-#Printing math table using user input
+
+# Task 1
+
+    #Printing math table using user input
 
 
 table = int(input("Enter a number "))
@@ -8,3 +11,40 @@ print(f"{table} * 3 = {table * 3}")
 print(f"{table} * 4 = {table * 4 }")
 print(f"{table} * 5 = {table * 5 }")
 print(f"{table} * 6 = {table * 6 }")
+print(f"{table} * 7 = {table * 7 }")
+print(f"{table} * 8 = {table * 8 }")
+print(f"{table} * 9 = {table * 9 }")
+print(f"{table} * 10 = {table * 10 }")
+
+
+# Task 2
+
+
+     #  mathemetical functions
+
+#calculator program
+
+number1 = int(input("Enter your number 1"))
+number2 = int(input("Enter your number 2"))
+"""
+number1_type= (type(number1))
+if number1_type == int :
+    print("INT")
+else :
+    print("None")
+"""
+max_value =max(number1,number2)
+Power_value = pow(number1,number2)
+usum= number1 + number2
+Difference = number1 - number2
+product = number1 * number2
+quotient = number1 / number2
+
+print(max_value)
+print(Power_value)
+print(usum)
+print(Difference)
+print(product)
+print(f"{quotient:.2f}")
+
+
